5ff16fafbfce3ea9d5390f20aa1598c158a1108a
Max Pozhidayev <pogidaevmo@gmail.com>
2017-10-20 21:49:59 +0300

    change angular-qs page

--- pages/angular-qs/main.bundle.js
+++ pages/angular-qs/main.bundle.js
@@ -20,7 +20,7 @@ webpackEmptyAsyncContext.id = "../../../../../src/$$_gendir lazy recursive";
 /***/ "../../../../../src/app/app.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<nav>\n  <div class=\"container\">\n    \n  <div class=\"nav-wrapper\">\n    <a href=\"#\" class=\"brand-logo\">Logo</a>\n    <ul id=\"nav-mobile\" class=\"right hide-on-med-and-down\">\n      <li><a routerLink=\"/\">Home</a></li>\n      <li><a routerLink=\"/setup\">SetUp</a></li>\n    </ul>\n  </div>\n  </div>\n</nav>\n\n\n<div class=\"main\">\n  <router-outlet></router-outlet>\n</div>"
+module.exports = "<nav>\n  <div class=\"container\">\n    \n  <div class=\"nav-wrapper\">\n    <a href=\"#\" class=\"brand-logo hide-on-med-and-down\">Logo</a>\n    <ul id=\"nav-mobile\" class=\"right \">\n      <li><a routerLink=\"/\">Home</a></li>\n      <li><a routerLink=\"/setup\">SetUp</a></li>\n    </ul>\n  </div>\n  </div>\n</nav>\n\n\n<div class=\"main\">\n  <router-outlet></router-outlet>\n</div>"
 
 /***/ }),
 
@@ -88,6 +88,8 @@ AppComponent = __decorate([
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__search_pipe__ = __webpack_require__("../../../../../src/app/search.pipe.ts");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__home_page_home_page_component__ = __webpack_require__("../../../../../src/app/home-page/home-page.component.ts");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_10__setup_page_setup_page_component__ = __webpack_require__("../../../../../src/app/setup-page/setup-page.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_11__not_found_not_found_component__ = __webpack_require__("../../../../../src/app/not-found/not-found.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_12__single_page_single_page_component__ = __webpack_require__("../../../../../src/app/single-page/single-page.component.ts");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -105,9 +107,13 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
+
+
 var appRoutes = [
     { path: '', component: __WEBPACK_IMPORTED_MODULE_9__home_page_home_page_component__["a" /* HomePageComponent */] },
-    { path: 'setup', component: __WEBPACK_IMPORTED_MODULE_10__setup_page_setup_page_component__["a" /* SetupPageComponent */] }
+    { path: ':id/:name/:geo/:img', component: __WEBPACK_IMPORTED_MODULE_12__single_page_single_page_component__["a" /* SinglePageComponent */] },
+    { path: 'setup', component: __WEBPACK_IMPORTED_MODULE_10__setup_page_setup_page_component__["a" /* SetupPageComponent */] },
+    { path: '**', component: __WEBPACK_IMPORTED_MODULE_11__not_found_not_found_component__["a" /* NotFoundComponent */] }
 ];
 var AppModule = (function () {
     function AppModule() {
@@ -121,13 +127,15 @@ AppModule = __decorate([
             __WEBPACK_IMPORTED_MODULE_6__card_card_component__["a" /* CardComponent */],
             __WEBPACK_IMPORTED_MODULE_8__search_pipe__["a" /* SearchPipe */],
             __WEBPACK_IMPORTED_MODULE_9__home_page_home_page_component__["a" /* HomePageComponent */],
-            __WEBPACK_IMPORTED_MODULE_10__setup_page_setup_page_component__["a" /* SetupPageComponent */]
+            __WEBPACK_IMPORTED_MODULE_10__setup_page_setup_page_component__["a" /* SetupPageComponent */],
+            __WEBPACK_IMPORTED_MODULE_11__not_found_not_found_component__["a" /* NotFoundComponent */],
+            __WEBPACK_IMPORTED_MODULE_12__single_page_single_page_component__["a" /* SinglePageComponent */]
         ],
         imports: [
             __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
             __WEBPACK_IMPORTED_MODULE_2__angular_http__["b" /* HttpModule */],
             __WEBPACK_IMPORTED_MODULE_3__angular_forms__["a" /* FormsModule */],
-            __WEBPACK_IMPORTED_MODULE_4__angular_router__["a" /* RouterModule */].forRoot(appRoutes)
+            __WEBPACK_IMPORTED_MODULE_4__angular_router__["b" /* RouterModule */].forRoot(appRoutes)
         ],
         providers: [__WEBPACK_IMPORTED_MODULE_7__user_service__["a" /* UserService */]],
         bootstrap: [__WEBPACK_IMPORTED_MODULE_5__app_component__["a" /* AppComponent */]]
@@ -141,7 +149,7 @@ AppModule = __decorate([
 /***/ "../../../../../src/app/card/card.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "\n\n\n<div class=\"col s12 m6 l4 xl3\" >\n\t<div class=\"card\" [ngClass]=\"{'marked' : mark}\">\n\t\t<div class=\"card-image\">\n\t\t\t<img src={{cardItem.img}}>\n\t\t\t<span class=\"card-title\">{{ cardItem.name }}</span>\n\t\t</div>\n\t\t<div class=\"card-content\">\n\t\t\t<p>{{ cardItem.geo }}</p>\n\t\t</div>\n\t\t<div class=\"card-action\">\n\t\t \t<a \n\t\t \t\t(click)=onClick()\n\t\t \t\t\n\t\t \t\tclass=\"waves-effect waves-light btn card__btn\">\n\t\t \t\t{{mark ? 'unselect' : 'select'}}\n\t\t \t</a>\n\t\t</div>\n\t</div>\n</div>"
+module.exports = "<div class=\"col s6 m4 l4 xl3\" >\n\t<div class=\"card\" [ngClass]=\"{'marked' : mark}\">\n\t\t<div class=\"card-image\">\n\t\t\t<img src={{cardItem.img}}>\n\t\t\t<span class=\"card-title\">{{ cardItem.name }}</span>\n\t\t</div>\n\t\t<div class=\"card-content\">\n\t\t\t<p>{{ cardItem.geo }}</p>\n\t\t</div>\n\t\t<div class=\"card-action\">\n\t\t \t<a \n\t\t \t\t(click)=onClick()\n\t\t \t\tclass=\"btn-floating btn-large waves-effect waves-light red\">\n\t\t \t\t<i class=\"material-icons\">{{ mark ? 'remove' : 'add' }}</i>\n\t\t \t</a>\n\t\t \t<a \n\t\t \t\t[routerLink]=\"[cardItem.id, cardItem.name, cardItem.geo, cardItem.img]\"\n\t\t \t\tclass=\"waves-effect waves-light btn card__btn\">\n\t\t \t\tmore\n\t\t \t</a>\n\t\t</div>\n\t</div>\n</div>"
 
 /***/ }),
 
@@ -276,6 +284,67 @@ var _a;
 
 /***/ }),
 
+/***/ "../../../../../src/app/not-found/not-found.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"container\">\n  \n  <h2>the page is not found</h2>\n  <p>404 error</p>\n\n</div>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/not-found/not-found.component.scss":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, ".container {\n  text-align: center; }\n\np {\n  color: #ee6e73;\n  font-size: 80px; }\n\nh2 {\n  text-transform: uppercase; }\n", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/not-found/not-found.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return NotFoundComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var NotFoundComponent = (function () {
+    function NotFoundComponent() {
+    }
+    NotFoundComponent.prototype.ngOnInit = function () {
+    };
+    return NotFoundComponent;
+}());
+NotFoundComponent = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["o" /* Component */])({
+        selector: 'app-not-found',
+        template: __webpack_require__("../../../../../src/app/not-found/not-found.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/not-found/not-found.component.scss")]
+    }),
+    __metadata("design:paramtypes", [])
+], NotFoundComponent);
+
+//# sourceMappingURL=not-found.component.js.map
+
+/***/ }),
+
 /***/ "../../../../../src/app/search.pipe.ts":
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
@@ -294,7 +363,7 @@ var SearchPipe = (function () {
     }
     SearchPipe.prototype.transform = function (users, val) {
         return users.filter(function (user) {
-            return user.name.includes(val);
+            return user.name.toLowerCase().includes(val.toLowerCase());
         });
     };
     return SearchPipe;
@@ -378,6 +447,78 @@ var _a;
 
 /***/ }),
 
+/***/ "../../../../../src/app/single-page/single-page.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"container\">\n  <img class=\"img\" src=\"{{img}}\" alt=\"{{name}}\">\n\n\n  <p class=\"name\">{{name}}</p>\n  <p class=\"geo\">{{geo}}</p>\n</div>\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/single-page/single-page.component.scss":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, ".name {\n  font-size: 30px;\n  text-transform: capitalize;\n  font-weight: bold; }\n\n.img {\n  width: 300px; }\n\n.geo {\n  font-style: italic;\n  text-transform: capitalize;\n  font-size: 20px; }\n", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/single-page/single-page.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return SinglePageComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_router__ = __webpack_require__("../../../router/@angular/router.es5.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+var SinglePageComponent = (function () {
+    function SinglePageComponent(route) {
+        this.route = route;
+    }
+    SinglePageComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.route.params.subscribe(function (params) {
+            _this.id = params['id'];
+            _this.name = params['name'];
+            _this.img = params['img'];
+            _this.geo = params['geo'];
+        });
+    };
+    return SinglePageComponent;
+}());
+SinglePageComponent = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["o" /* Component */])({
+        selector: 'app-single-page',
+        template: __webpack_require__("../../../../../src/app/single-page/single-page.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/single-page/single-page.component.scss")]
+    }),
+    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1__angular_router__["a" /* ActivatedRoute */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1__angular_router__["a" /* ActivatedRoute */]) === "function" && _a || Object])
+], SinglePageComponent);
+
+var _a;
+//# sourceMappingURL=single-page.component.js.map
+
+/***/ }),
+
 /***/ "../../../../../src/app/user.service.ts":
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
@@ -405,12 +546,13 @@ var UserService = (function () {
         this.amount = 8;
     }
     UserService.prototype.getUsers = function () {
-        return this.http.get('https://randomuser.me/api/?inc=gender,name,picture,location&results=' + this.amount + '&nat=gb')
+        return this.http.get('https://randomuser.me/api/?inc=id,gender,name,picture,location&results=' + this.amount + '&nat=gb')
             .map(function (response) { return response.json(); })
             .map(function (response) { return response.results; })
             .map(function (users) {
             return users.map(function (u) {
                 return {
+                    id: u.id.value,
                     name: u.name.first + " " + u.name.last,
                     img: u.picture.large,
                     geo: u.location.city + ' ' + u.location.state + ' ' + u.location.street
